8b7391618bf9021d560afee15cd7ad27629c975b
Ralf S. Engelschall <rse@engelschall.com>
2013-11-27 00:22:44 +0100

    simplify code

--- app/app-ui-main-view.js
+++ app/app-ui-main-view.js
@@ -1,13 +1,12 @@
 
 cs.ns("app.ui.main").view = cs.clazz({
     mixin: [ cs.marker.view ],
-    dynamics: { ui: null },
     protos: {
         render: function () {
-            var self = this
-            self.ui = $.markup("main")
-            cs(self).plug(self.ui)
-            cs(self).socket({ ctx: $("#todoapp", self.ui), spool: "materialized" });
+            /*  render view mask  */
+            var ui = $.markup("main")
+            cs(this).plug(ui)
+            cs(this).socket({ ctx: $("#todoapp", ui), spool: "materialized" });
         }
     }
 })
